04b43e56710f265eb5fa8d5bdbe2852d5246adb2
Shaurya Kushwaha <shaurya08k@gmail.com>
2026-09-06 01:25:17 +0530

    feat: add Navbar with React Router links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Societies from "./pages/Societies";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/societies" element={<Societies />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/societies" element={<Societies />} />
+      </Routes>
+    </>
   );
 }
 
